2732ae6e7e96f2145845babc4b656474d7c26c29
Andrew Ha <andrewha428@gmail.com>
2020-01-12 20:38:22 -0500

    added state and function for fetching dining names and locations from backend

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -3,16 +3,41 @@ import Card from '../Card/Card';
 import './CardContainer.css';
 
 class CardContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            schoolName: props.schoolName,
+            nameLocation: []
+        }
+
+        this.getLocations(this.state.schoolName);
+    }
+
+    getLocations = (schoolName) => {
+        fetch('http://localhost:3000/getLocations/' + schoolName, {
+          method: 'get',
+          headers: { 'Content-type': 'application/json' },
+        })
+          .then(response => response.json())
+          .then(response => {this.setState({nameLocation: response})})
+          .catch(err => console.log(err))
+      }
+
     render() {
+        // console.log(this.getLocations(this.state.schoolName));
+
         return (
-            <div className="justify-center flex">
-                <div className="flex flex-wrap pt3 pl3 pr3 ml5 mr5 mt1 inline-flex">
+            <div className="justify-center ">
+                <div className=" flex flex-wrap  pt3 pl3 pr3 ml5 mr5 mt1">
+                    {/* <Card schoolName={this.state.schoolName}/>
                     <Card />
                     <Card />
                     <Card />
                     <Card />
-                    <Card />
-                    <Card />
+                    <Card /> */}
+                    {this.state.nameLocation.map((nameLocation, keyID) => {
+                        return <Card diningName={nameLocation.dining_name} locationName={nameLocation.location_name} key={keyID} />
+                    })}
                 </div>
             </div>
 
