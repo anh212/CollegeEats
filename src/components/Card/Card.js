f4b7b64855648fc05dd920541c980431d3bb4ffc
Andrew Ha <andrewha428@gmail.com>
2020-01-12 20:37:14 -0500

    added state for each card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,15 +2,30 @@ import React from 'react';
 import './Card.css';
 
 class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      diningName: this.props.diningName,
+      location: this.props.locationName,
+      isOpen: true,
+      schedule: [],
+
+    }
+  }
+
+  getSchedule = (diningName) => {
+
+  }
+
   render() {
     return (
       <div className="card pa3 mb3 mr3">
         <h5 className="diningName mt0 mb2">
-          <span className="">Dining Name</span>
+          <span className="">{this.state.diningName}</span>
           <span className="br-100 mb0 ml2 indicatorLight openPlace"></span>
         </h5>
-          <h6 className="mb2 mt0 location">Location</h6>
-          <p className="ma0">When does it close: asokfsa[laspd</p>
+        <h6 className="mb2 mt0 location">{this.state.location}</h6>
+        <p className="ma0">When does it close: asokfsa[laspd</p>
       </div>
     );
   }
